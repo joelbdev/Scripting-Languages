7e486ebe922dd12ca23e9c2d842085a787a14d60
joelbdev <61047757+joelbdev@users.noreply.github.com>
2020-06-07 13:12:53 +1000

    Update downloader.sh

--- week3/downloader.sh
+++ week3/downloader.sh
@@ -5,7 +5,7 @@ echo "Website downloader - this script will keep downloading a webpage until you
 while true #default condition is true - therefore script will keep looping
 do
     read -p "What website do you want to download? " website
-    if [ $website != "exit" ];then
+    if [ $website != "exit" ];then #if the user enters exit, it will go to the else condition and exit
         read -p "Where do you want to save the webpage? " directory 
         wget -P $directory $website        
     else
