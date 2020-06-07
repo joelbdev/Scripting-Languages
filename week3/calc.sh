1794af2ca6cb0f11bba7ecd977c260c0a756c580
joelbdev <61047757+joelbdev@users.noreply.github.com>
2020-06-07 13:11:38 +1000

    Update calc.sh

--- week3/calc.sh
+++ week3/calc.sh
@@ -6,7 +6,7 @@ read -p "Enter second number: " num2
 
 read -p "Enter operator choice: " operator
 
-case $operator in
+case $operator in #performs the arithmatic operations on the two numbers based on what operator the user selects
     +) result=$(expr $num1 + $num2) && echo -e "\033[34m $result"
     ;;
     -) result=$(expr $num1 - $num2) && echo -e "\033[32m $result"
