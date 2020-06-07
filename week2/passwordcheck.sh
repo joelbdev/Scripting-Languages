d4c66fab924283e2c2aacabb3fb66be12bac422e
joelbdev <61047757+joelbdev@users.noreply.github.com>
2020-06-07 13:09:20 +1000

    Update passwordcheck.sh

--- week2/passwordcheck.sh
+++ week2/passwordcheck.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-read -sp "Type a password: " password
-passwordHash=$(echo "$password" | sha256sum)
-storedpassword=$(< password.txt)
+read -sp "Type a password: " password #request the user to supply a password
+passwordHash=$(echo "$password" | sha256sum) #takes the user supplied string and hashes it
+storedpassword=$(< password.txt) #takes the stored hash from password.txt and stores as a variable
 
-if [ "$passwordHash" == "$storedpassword" ];then
-   echo -e "\n \033[32m Access granted" #-e modifier so that string appears on new line
+if [ "$passwordHash" == "$storedpassword" ];then #compares the hash of the password entered to the hash of the password on file
+   echo -e "\n \033[32m Access granted" #-e modifier so that new line character can be used for better formatting
 else
     echo -e " \n \033[031m Access denied"
 fi
