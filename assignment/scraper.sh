0f9a2299788cbff351866ba8468eeb2f876745b2
joel <joelbdev@gmail.com>
2020-06-19 09:33:31 +1000

    Finalisation of assignment code

--- assignment/scraper.sh
+++ assignment/scraper.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-x=1
-#set up the CSV file to recieve the parsed content from the HTML file
-
-#echo "Date,Title,Description,Tag,Author,Link" > extractData.csv
 
 #While loop to iterate over the first 5 pages of the website
-while [[ $x -le 5 ]]; do
+x=1
+echo -e "\033[31m Scraping Packetstorm Security for latest exploits!\033[0m"
+
+while [[ $x -le 2 ]]; do
 
 #download each page of the website
-
 curl https://packetstormsecurity.com/files/tags/exploit/page$x/ > analysisfile
 
-#parse the HTML file to pull out the fields
+#parse the HTML file to pull out the required fields and use sed to clean up the text
 
 dates=$(cat analysisfile | grep 'datetime' | cut -d'>' -f3 | sed -e 's!</a!''!g' -e 's!,!''!g' -e 's!  ! !g' | cat >> ./data/dates.csv) #cleans the date format by removing '</a', ',' and the double space
 titles=$(cat analysisfile | grep '.text-plain' | cut -d'>' -f3 | sed 's!</a!''!g' | cat >> ./data/titles.csv) #isolate the titles of each exploit and remove the '</a'
@@ -19,11 +17,6 @@ titles=$(cat analysisfile | grep '.text-plain' | cut -d'>' -f3 | sed 's!</a!''!g
 tags=$(cat analysisfile | grep 'class="tags"' | cut -d'>' -f7 | sed 's!</a!''!g' | sed 's!</p!''!g' | cat >> ./data/tags.csv)  #isolate the tags of each exploit and remove the '</a' #TODO probably need to do something with these tags as there are multiple
 authors=$(cat analysisfile | grep 'person' | cut -d'>' -f3 | sed 's!</a!''!g'| sed 's!</p!''!g' | cat >> ./data/authors.csv) #isolate the authors and remove the '</a'
 links=$(cat analysisfile| grep 'ico text-plain' | cut -d'>' -f2 | cut -d'"' -f4 | sed 's!^!https://packetstormsecurity.com!' | cat >> ./data/links.csv) #converts relative link to absolute url
-
-#append scraped fields to the csv
-#TODO: Do I need to use awk to get them into different columns?
-
-#echo "${dates},${titles},${descriptions},${authors},${links}" >> extractData.csv
 
 #increment the x variable to register what page is being scraped
 let "x=x+1"
@@ -35,4 +28,6 @@ eog ./wordcloud.png #open the wordcloud
 
 #cleaning up after itself
 rm ./data/*.csv
-#TODO put the parsing within a function
+rm analysisfile
+
+
