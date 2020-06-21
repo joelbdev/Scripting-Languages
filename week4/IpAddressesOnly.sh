f42b66b75a44d6f0e0a5d35642c16f84347819e3
Joel <joelbdev@gmail.com>
2020-06-21 16:05:57 +1000

    Changed IpAddressesonly.sh to add extra code in case we were not meant to just filter the output of IpInfo.sh

--- week4/IpAddressesOnly.sh
+++ week4/IpAddressesOnly.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 ./IpInfo.sh | sed -n '/IP Address/'p
+
+#I have taken the creative freedom to assume that it is okay to filter the output of the IpInfo.sh script to display only the IP addresses.
+
+#If this is not meant to be the case and we are meant to create a whole new script, the following code will satisfy the requirements:
+
+ #net_info="$(ifconfig)"
+ #addresses=$(echo "$net_info" | sed -n '/inet / {
+ #s/inet/IP Address:/
+ #s/netmask/\n\t\tSubnet Mask:/ 
+ #s/broadcast/\n\t\tBroadcast Address:/
+ #p
+ #}')
+ #format output
+ #echo -e "$addresses" | grep "IP Address"
